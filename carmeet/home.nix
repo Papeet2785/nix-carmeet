90cde9d3fc1995d898e7ee326aec1d4080c24414
Papeet2785 <carmeet2785@gmail.com>
2026-08-10 21:54:37 -0400

    cleaned up fastfetch stuff

--- carmeet/home.nix
+++ carmeet/home.nix
@@ -26,9 +26,9 @@
     ./dotfiles/fish.nix
     ./dotfiles/xdg-stuff.nix
     ./dotfiles/helix.nix
+    ./dotfiles/fastfetch.nix
   ];
   home.file = {
-  ".config/fastfetch".source = ./dotfiles/fastfetch;
   ".config/niri".source = ./dotfiles/niri;
   };
 }
