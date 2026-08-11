9af9666d55032b8a092713dec35a630f8e3bf72d
Papeet2785 <carmeet2785@gmail.com>
2026-08-10 21:24:55 -0400

    added xdg-stuff.nix

---
+++ carmeet/dotfiles/xdg-stuff.nix
@@ -0,0 +1,69 @@
+
+{ config, pkgs, lib, ... }: {
+
+  xdg.desktopEntries.arduino = {
+    name = "Arduino IDE";
+    exec = "appimage-run /home/carmeet/Applications/arduino-ide_2.3.10_Linux_64bit.AppImage %F";
+    terminal = false;
+    categories = [ "Development" "IDE" ];
+  };
+
+  xdg.desktopEntries.processing = {
+    name = "Processing";
+    exec = "env _JAVA_AWT_WM_NONREPARENTING=1 GDK_BACKEND=x11 /home/carmeet/Applications/Processing/bin/Processing %F";
+    icon = "/home/carmeet/Applications/Processing/lib/Processing.png";
+    terminal = false;
+    categories = [ "Development" "IDE" ];
+  };
+
+  home.file.".local/share/applications/helix-terminal.desktop".text = ''
+    [Desktop Entry]
+    Name=Helix
+    Comment=Helix in Kitty
+    Exec=kitty -e hx %F
+    Terminal=false
+    Type=Application
+    MimeType=text/plain;text/markdown;text/x-c;text/x-java;text/html;text/css;text/javascript;application/json;text/x-python;text/x-nix;
+  '';
+
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      # Browser
+      "text/html" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+
+      # Text / programming
+      "text/plain" = "helix-terminal.desktop";
+      "text/markdown" = "helix-terminal.desktop";
+      "text/x-c" = "helix-terminal.desktop";
+      "text/x-java" = "helix-terminal.desktop";
+      "text/x-python" = "helix-terminal.desktop";
+      "text/x-nix" = "helix-terminal.desktop";
+      "text/css" = "helix-terminal.desktop";
+      "text/javascript" = "helix-terminal.desktop";
+      "application/json" = "helix-terminal.desktop";
+
+      # Arduino
+      "text/x-arduino" = "arduino.desktop";
+
+      # Processing
+      "text/x-processing" = "processing.desktop";
+
+      # PDF
+      "application/pdf" = "org.gnome.Evince.desktop";
+
+      # Microsoft Office
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" =
+        "onlyoffice-desktopeditors.desktop";
+
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" =
+        "onlyoffice-desktopeditors.desktop";
+
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" =
+        "onlyoffice-desktopeditors.desktop";
+    };
+  };
+}
--- carmeet/home.nix
+++ carmeet/home.nix
@@ -1,4 +1,3 @@
-
 { config, pkgs, lib, inputs, ... }: {
   home.username = "carmeet";
   home.homeDirectory = "/home/carmeet";
@@ -25,101 +24,12 @@
   };
   imports = [
     ./dotfiles/fish.nix
+    ./dotfiles/xdg-stuff.nix
   ];
   home.file = {
   ".config/helix".source = ./dotfiles/helix;
   ".config/fastfetch".source = ./dotfiles/fastfetch;
   ".config/niri".source = ./dotfiles/niri;
   ".config/noctalia".source = ./dotfiles/noctalia;
-
-  ".local/share/applications/helix-terminal.desktop".text = ''
-    [Desktop Entry]
-    Name=Helix
-    Comment=Helix in Ghostty
-    Exec=ghostty -e hx %F
-    Terminal=false
-    Type=Application
-    Categories=Development;TextEditor;
-  '';
-
-  ".local/share/mime/packages/carmeet-mime.xml".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-      <mime-type type="text/x-arduino">
-        <comment>Arduino Sketch</comment>
-        <glob pattern="*.ino"/>
-      </mime-type>
-
-      <mime-type type="text/x-processing">
-        <comment>Processing Sketch</comment>
-        <glob pattern="*.pde"/>
-      </mime-type>
-    </mime-info>
-  '';
-  };
-
-  xdg.desktopEntries.arduino = {
-    name = "Arduino IDE";
-    exec = "appimage-run /home/carmeet/Applications/arduino-ide_2.3.10_Linux_64bit.AppImage %F";
-    terminal = false;
-    categories = [ "Development" "IDE" ];
-    mimeType = [ "text/x-arduino" ];
-  };
-
-  xdg.desktopEntries.processing = {
-    name = "Processing";
-    exec = "env _JAVA_AWT_WM_NONREPARENTING=1 GDK_BACKEND=x11 /home/carmeet/Applications/Processing/bin/Processing %F";
-    icon = "/home/carmeet/Applications/Processing/lib/Processing.png";
-    terminal = false;
-    categories = [ "Development" "IDE" ];
-    mimeType = [ "text/x-processing" ];
-  };
-
-  xdg.mimeApps = {
-    enable = true;
-
-    defaultApplications = {
-      # Browser
-      # Text / programming
-      "text/plain" = "helix-terminal.desktop";
-      "text/markdown" = "helix-terminal.desktop";
-      "text/x-c" = "helix-terminal.desktop";
-      "text/x-java" = "helix-terminal.desktop";
-      "text/x-python" = "helix-terminal.desktop";
-      "text/x-nix" = "helix-terminal.desktop";
-      "text/css" = "helix-terminal.desktop";
-      "text/javascript" = "helix-terminal.desktop";
-      "application/json" = "helix-terminal.desktop";
-
-      # Images
-      "image/png" = "org.gnome.Shotwell-Viewer.desktop";
-      "image/jpeg" = "org.gnome.Shotwell-Viewer.desktop";
-      "image/gif" = "org.gnome.Shotwell-Viewer.desktop";
-      "image/webp" = "org.gnome.Shotwell-Viewer.desktop";
-      "image/tiff" = "org.gnome.Shotwell-Viewer.desktop";
-      "image/bmp" = "org.gnome.Shotwell-Viewer.desktop";
-
-      # Arduino
-      "text/x-arduino" = "arduino.desktop";
-
-      # Processing
-      "text/x-processing" = "processing.desktop";
-
-      # Images
-      "application/pdf" = "org.gnome.Evince.desktop";
-
-      # Directories
-      "inode/directory" = "org.gnome.Nautilus.desktop";
-
-      # Microsoft Office documents
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "onlyoffice-desktopeditors.desktop";
-      "application/msword" = "onlyoffice-desktopeditors.desktop";
-
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "onlyoffice-desktopeditors.desktop";
-      "application/vnd.ms-excel" = "onlyoffice-desktopeditors.desktop";
-
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = "onlyoffice-desktopeditors.desktop";
-      "application/vnd.ms-powerpoint" = "onlyoffice-desktopeditors.desktop";
-    };
   };
 }
