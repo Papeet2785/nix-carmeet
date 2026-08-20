4bdbca09ef9a5a6f41ee56bdb443af6174ed9a27
Papeet2785 <carmeet2785@gmail.com>
2026-08-20 12:57:56 -0400

    made font size more consistent

--- carmeet/theme.nix
+++ carmeet/theme.nix
@@ -31,9 +31,9 @@
         name = "Noto Color Emoji";
       };
       sizes = {
-        applications = 12;
-        terminal = 12;
-        desktop = 12;
+        applications = 10;
+        terminal = 14;
+        desktop = 10;
         popups = 12;
       };
     };
