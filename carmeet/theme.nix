8aa133584e9818426a909c2c3adb041e8bd73f37
Papeet2785 <carmeet2785@gmail.com>
2026-08-20 12:50:38 -0400

    changing all sys fonts to jetbrainsmono

--- carmeet/theme.nix
+++ carmeet/theme.nix
@@ -15,25 +15,25 @@
     };
     fonts = {
       monospace = {
-        package = pkgs.nerd-fonts.meslo-lg;
-        name = "MesloLGM Nerd Font";
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
       };
       serif = {
-        package = pkgs.comfortaa;
-        name = "Comfortaa";
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
       };
       sansSerif = {
-        package = pkgs.comfortaa;
-        name = "Comfortaa";
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
       };
       emoji = {
         package = pkgs.noto-fonts-color-emoji;
         name = "Noto Color Emoji";
       };
       sizes = {
-        applications = 10;
+        applications = 12;
         terminal = 12;
-        desktop = 10;
+        desktop = 12;
         popups = 12;
       };
     };
