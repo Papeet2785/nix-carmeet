293bcdbeef3c0cba2db87d294b256e16d5f694c6
Papeet2785 <carmeet2785@gmail.com>
2026-08-21 17:23:25 -0400

    changed niri border color to that of noctalia-shell fg

--- carmeet/dotfiles/niri.nix
+++ carmeet/dotfiles/niri.nix
@@ -33,7 +33,7 @@
       focus-ring = {
         enable = true;
         width = 4;
-        active.color = "#ebdbb2";
+        active.color = "#fbf1c7";
       };
       preset-column-widths = [
         { proportion = 0.50; }
