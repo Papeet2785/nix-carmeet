7448dcccb40976ca9905a445ea222b1e51b0966c
Papeet2785 <carmeet2785@gmail.com>
2026-09-23 15:39:15 -0400

    changed fg color

--- carmeet/dotfiles/niri.nix
+++ carmeet/dotfiles/niri.nix
@@ -42,7 +42,7 @@
       focus-ring = {
         enable = true;
         width = 3;
-        active.color = "#abb2bf";
+        active.color = "#ebdbb2";
       };
       preset-column-widths = [
         { proportion = 0.50; }
