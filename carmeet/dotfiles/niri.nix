c234f0f96dca69fa02922be3b80d934e68708268
Papeet2785 <carmeet2785@gmail.com>
2026-09-23 15:43:43 -0400

    changing binds for helix

--- carmeet/dotfiles/niri.nix
+++ carmeet/dotfiles/niri.nix
@@ -72,8 +72,8 @@
       # Applications
       "Mod+Return".action.spawn = "kitty";
       "Mod+Escape".action.spawn = [ "kitty" "-e" "btop" ];
-      "Mod+C".action.spawn = [ "kitty" "--working-directory" "/home/carme" "-e" "hx" ];
-      "Mod+Shift+C".action.spawn = [ "kitty" "--working-directory" "/home/carme/nix-carmeet" "-e" "hx" ];
+      "Mod+C".action.spawn = [ "kitty" "-e" "hx /home/carmeet" ];
+      "Mod+Shift+C".action.spawn = [ "kitty" "-e" "hx /home/carmeet/nix-carmeet" ];
       "Mod+Tab".action.toggle-overview = {};
       "Mod+B".action.spawn = "firefox";
       "Mod+Shift+B".action.spawn = "google-chrome";
