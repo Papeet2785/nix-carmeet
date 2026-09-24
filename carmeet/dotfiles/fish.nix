a451aa394f566a2847c044c495a111668f8fe75d
Papeet2785 <carmeet2785@gmail.com>
2026-09-24 17:19:55 -0400

    customized starship prompt lil bit

--- carmeet/dotfiles/fish.nix
+++ carmeet/dotfiles/fish.nix
@@ -8,6 +8,9 @@
       rm = "trash -v";
       mkdir = "mkdir -p";
       la = "ls -Alh";
+      nixbuild = "cd nix-carmeet && sudo nixos-rebuild switch --flake .#myMachine";
+      nixupgrade = "cd nix-carmeet && nix flake update && sudo nixos-rebuild switch --flake .#myMachine";
+      fetch = "fastfetch";
     };
      functions = {
       fish_greeting = "";
@@ -16,5 +19,8 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      format = "$directory$character ";
+    };
   };
 }
