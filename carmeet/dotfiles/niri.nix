ba52105c498578ddbc94c71248e8ac8b6f6adb27
Papeet2785 <carmeet2785@gmail.com>
2026-09-26 08:42:43 -0400

    border clr change

--- carmeet/dotfiles/niri.nix
+++ carmeet/dotfiles/niri.nix
@@ -42,7 +42,7 @@
       focus-ring = {
         enable = true;
         width = 3;
-        active.color = "#458588";
+        active.color = "#7fa2ac";
       };
       preset-column-widths = [
         { proportion = 0.50; }
